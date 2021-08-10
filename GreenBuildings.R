ed1b39dee3a17afce1d1f45c94c31687f229dc65
gsooter <87453455+gsooter@users.noreply.github.com>
2021-08-10 14:09:44 -0500

    Final green buildings

--- GreenBuildings.R
+++ GreenBuildings.R
@@ -1,17 +1,13 @@
-install.packages('vioplot')
 library(mosaic)
-library(vioplot)
 green = read.csv('greenbuildings.csv')
 #green = subset(green, leasing_rate > 40)
 summary(green)
 
 plot(green$leasing_rate, green$Rent)
 
-
-lm1 = lm(green$Rent ~ green$leasing_rate)
-summary(lm1)
-hist(green$leasing_rate)
-
+ggplot(green_only) + 
+  geom_point(aes(x=cluster_rent, y=Rent, color = class_a))+
+  labs(x = 'Cluster Rent ($ per sq ft)', y = 'Green Buliding Rent ($ per sq ft)', title = 'Green Building Rent vs Associated Cluseter Rent')
 
 # Extract the buildings with green ratings
 green_only = subset(green, green_rating==1)
@@ -22,113 +18,102 @@ dim(non_green)
 hist(green_only$Rent, 25)
 mean(green_only$Rent)
 
+rents <- green_only$Rent + non_green$Rent
+
 hist(non_green$Rent, 25)
 mean(non_green$Rent)
 
-# Using R's lm function
-model1 = lm(Rent ~ 1, data=green_only)
-confint(model1, level=0.95)
+################################
 
-model1 = lm(Rent ~ 1, data=non_green)
-confint(model1, level=0.95)
-### Compare with bootstrapping
+# finding the mean of the non green and green split by A and B
 
-# Now repeat 2500 times
+################################
+a_only = subset(green, class_a==1)
+b_only = subset(green, class_b==1)
+
+median(a_only$Rent)
+median(b_only$Rent)
+mean(a_only$Rent)
+mean(b_only$Rent)
+hist(a_only$Rent)
+hist(b_only$Rent)
+
+############ over all the rent is much higher in A or the premium area
+
+green_onlya = subset(a_only, green_rating==1)
+non_greena = subset(a_only, green_rating!=1)
+
+median(green_onlya$Rent)
+median(non_greena$Rent)
+mean(green_onlya$Rent)
+mean(non_greena$Rent)
+hist(green_onlya$Rent)
+hist(non_greena$Rent)
+
 boot1 = do(2500)*{
-  mean(resample(green_only)$Rent)
+  mean(resample(green_onlya)$Rent)
 }
 head(boot1)
 hist(boot1$result, 30)
 sd(boot1$result)
 mean(boot1$result)
 
+
 boot2 = do(2500)*{
-  mean(resample(non_green)$Rent)
+  mean(resample(non_greena)$Rent)
 }
 head(boot2)
 hist(boot2$result, 30)
 sd(boot2$result)
 mean(boot2$result)
+###### Non green is worth more in the A area
 
+green_onlyb = subset(b_only, green_rating==1)
+non_greenb = subset(b_only, green_rating!=1)
 
-# Extract the confidence interval from the bootstrapped samples
-confint(boot1, level=0.95)
+median(green_onlyb$Rent)
+median(non_greenb$Rent)
+mean(green_onlyb$Rent)
+mean(non_greenb$Rent)
+hist(green_onlyb$Rent)
+hist(non_greenb$Rent)
 
-xbar + c(-1.96,1.96)*se_hat
-
-# Calculate the mean and median of the bootstrap and see the median is much higher
-mean(boot1$result)
-median(boot1$result)
-
-####
-# Bootstrap the median
-####
-
-median(green_only$Rent)
-# Now repeat 2500 times
 boot3 = do(2500)*{
-  median(resample(green_only)$Rent)
+  mean(resample(green_onlyb)$Rent)
 }
 head(boot3)
+hist(boot3$result, 30)
+sd(boot3$result)
 mean(boot3$result)
 
+
 boot4 = do(2500)*{
-  median(resample(non_green)$Rent)
+  mean(resample(non_greenb)$Rent)
 }
 head(boot4)
+hist(boot4$result, 30)
+sd(boot4$result)
 mean(boot4$result)
+############ in area be you see that the rent is higher for the green buildings like hypothesized
+nrow(green_onlya)/nrow(a_only)*100
+
+nrow(green_onlyb)/nrow(b_only)*100
+# A much higher proportion of green are in the A which raises the average price
+## plotting
+
+hist(boot1$result, 45, col = 'green', xlab = 'Rent($ per sqft)', main = 'Green bulidings and non green buildings in the prime location')
+hist(boot2$result, 45, col = 'blue', add = T)+
+  legend("right", c("Green", "Non_green"), fill=c("green", "blue"))
+
+hist(boot3$result, 30, col = 'green', xlab = 'Rent($ per sqft)', main = 'Green bulidings and non green buildings in the lesser location')
+hist(boot4$result, 30, col = 'blue', add = T)+
+  legend("right", c("Green", "Non_green"), fill=c("green", "blue"))
 
 
+hist(non_greena$Rent, xlab = 'Mean Rent for location A', ylab = 'Frequency', col = 'blue', main = 'Relative frequency of green to non greeen buildings in group A', breaks = 25)
+hist(green_onlya$Rent, xlab = 'Mean Rent for location A', ylab = 'Frequency', col = 'green', breaks = 25, add = T)
 
-# Ugly!
-hist(boot2$result, 30)
-
-# But we still get a confidence interval
-x1 <- confint(boot3)
-x2 <- confint(boot4)
-
-vioplot(x1, x2)
-boxplot(boot3,boot4)
-boxplot(boot4)
-####### Now we begin our own visual analysis ######
-###### Start with plots ######
-
-library(tidyverse)
-
-#plot rent vs leasing rate
-ggplot(green) + 
-  geom_point(aes(x=Rent, y=leasing_rate, color=green_rating))
-
-ggplot(green_only) + 
-  geom_point(aes(x=Rent, y=leasing_rate))
-
-#plot the Gas and Electricity costs to rent
-#shows weak positive correlation to electricity and rent prices
-ggplot(green_only) + 
-  geom_point(aes(x=Electricity_Costs, y=Rent))
-
-ggplot(green_only) + 
-  geom_point(aes(x=Gas_Costs, y=Rent))
+hist(non_greenb$Rent, xlab = 'Mean Rent for location B', ylab = 'Frequency', col = 'blue', main = 'Relative frequency of green to non greeen buildings in group B', breaks = 25)
+hist(green_onlyb$Rent, xlab = 'Mean Rent for location B', ylab = 'Frequency', col = 'green',  breaks = 25, add = T)
 
 
-#cluster rent vs rent
-#shows a strong relation ship that the rent of a green building
-#has to the rent of non-green buildings around it
-#will be very difficult to disprove this as a confounding variable
-#additionally previous analysis excluded out liers (high rent variables)
-ggplot(green_only) + 
-  geom_point(aes(x=cluster_rent, y=Rent, color=class_a))
-
-#rent to age and size, no correlation
-ggplot(green_only) + 
-  geom_point(aes(x=stories, y=Rent))
-
-#rent to total days of heating and cooling needed, no correlation
-#could almost argue negative correlation
-#in fact one random area of with same high amt of heat/cool shows its probably more abt area
-ggplot(green) + 
-  geom_point(aes(x=total_dd_07, y=Rent, color=green_rating))
-
-#rent to class A showing if its green or not
-ggplot(green) + 
-  geom_point(aes(x=total_dd_07, y=Rent, color=green_rating))
